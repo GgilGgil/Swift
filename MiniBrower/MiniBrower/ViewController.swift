cfc31b8027bf9e6428b89e0bd00eda0bac8ef145
Park GilNam <knzzang7845@gmail.com>
2017-12-15 15:33:24 +0900

    MiniBrower Init page commit

--- MiniBrower/MiniBrower/ViewController.swift
+++ MiniBrower/MiniBrower/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var bookMarkSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var mainWebView: WKWebView!
+    @IBOutlet weak var spinningActivityIndicatorView: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let initURL = "https://www.facebook.com"
+        let myURL = URL(string: initURL)
+        let myRequest = URLRequest(url: myURL!)
+        mainWebView.load(myRequest)
     }
-
+    
+    @IBAction func bookMarkAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func goBackAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func goForwordAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func stopLoadingAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func reloadAction(_ sender: Any) {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
