a17fd2ccb12e7b3d51114764106418ad9b54a358
Park GilNam <knzzang7845@gmail.com>
2017-12-22 16:38:48 +0900

    FlightBooking UI commit

---
+++ FlightBooking/FlightBooking/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  FlightBooking
+//
+//  Created by Park GilNam on 2017. 12. 22..
+//  Copyright © 2017년 ParkGilNam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var departureDateButton: UIButton!
+    @IBOutlet weak var returnDateButton: UIButton!
+    @IBOutlet weak var returnDateLabel: UILabel!
+    @IBOutlet weak var selectDatePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func showDatePickerAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func showReturnDateAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func selectedDateAction(_ sender: Any) {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
