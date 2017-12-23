2bca7a8ca0764c909b424b01e159d59c028b8720
Park GilNam <knzzang7845@gmail.com>
2017-12-23 18:18:18 +0900

    XMLParser

---
+++ XMLParser/XMLParser/WeatherCell.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherCell.swift
+//  XMLParser
+//
+//  Created by Park GilNam on 2017. 12. 23..
+//  Copyright © 2017년 ParkGilNam. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var imgView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
